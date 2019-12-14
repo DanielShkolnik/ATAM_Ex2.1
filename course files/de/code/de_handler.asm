ccb1b6ea961d7b2c77f99e96e7aa9640470cd471
root <omer.hemo8000@gmail.com>
2019-12-14 13:20:09 +0000

    handler

---
+++ course files/de/code/de_handler.asm
@@ -0,0 +1,24 @@
+.global my_de_handler, my_get_interrupts_counter
+.extern #TODO: fill externals
+
+.data
+#TODO: define your own variables
+
+.text
+.align 8, 0x90
+my_de_handler:
+  #TODO: implement your own divide error handler
+  # When calling an interrupt handler, Intel pushes the following to the stack:
+  # * SS
+  # * RSP
+  # * RFLAGS
+  # * CS
+  # * RIP
+  # * (errorcode for several interrupts, but not for divide error)
+  
+  iretq
+  
+my_get_interrupts_counter:
+  #TODO: implement :)
+  xor %eax, %eax
+  ret
