b64709952e5f33c41909fe59897f50106cce21e4
root <omer.hemo8000@gmail.com>
2019-12-14 16:29:50 +0000

    de utils

--- course files/de/code/de_utils.c
+++ course files/de/code/de_utils.c
@@ -2,19 +2,44 @@
 
 void my_store_idt(struct desc_ptr *idtr) {
 	//TODO: store current IDTR in memory adress idtr
-	//asd
+	asm("sidt %0;"
+			:"r"(idtr) // input
+			:"%idtr" // clobbered reg
+		);
 }
 
 void my_load_idt(struct desc_ptr *idtr) {
 	//TODO: load data in memory address idtr to IDTR
+	asm("lidt %0;"
+			:"r"(idtr) // input
+			:"%idtr" // clobbered reg
+		);
+
 }
 
 void my_set_gate_offset(gate_desc *gate, unsigned long addr) {
 	//TODO: set gate address to addr
 	//      gate layout can be found here: https://wiki.osdev.org/Interrupt_Descriptor_Table
+	asm("mov %%bx, (%0);
+			 shr $16, %%rbx;
+			 mov %%bx, 6(%0);
+			 shr $16, %%rbx;
+			 movl %%ebx, 8(%0);"
+			:"r"(gate) // input
+			:"b"(addr) // input
+		);
 }
 
 unsigned long my_get_gate_offset(gate_desc *gate) {
 	//TODO: get gate address
-	return 0;
+	unsigned long addr = 0;
+	asm("movl 8(%1), %%ebx;
+			 sal $16, %%rbx;
+			 mov 6(%1), %%bx;
+			 sal $16, %%rbx;
+			 mov (%1), %%bx;"
+			:"=b"(addr) // input
+			:"r"(gate) // input
+		);
+	return addr;
 }
