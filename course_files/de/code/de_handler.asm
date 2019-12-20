7cbe23c2992e07f5721299e05822ae5ad3bd1a95
root <omer.hemo8000@gmail.com>
2019-12-20 23:31:42 +0000

    fgh

--- course_files/de/code/de_handler.asm
+++ course_files/de/code/de_handler.asm
@@ -1,10 +1,8 @@
 .global my_de_handler, my_get_interrupts_counter
-.extern what_to_do, old_de_handler #TODO: fill externals
+.extern what_to_do, old_de_handler, print #TODO: fill externals
 
 .data
-#TODO: define your own variables
-counter: .long 0
-
+counter: .space 8
 .text
 .align 8, 0x90
 my_de_handler:
@@ -16,48 +14,29 @@ my_de_handler:
   # * CS
   # * RIP
   # * (errorcode for several interrupts, but not for divide error)
+
   pushq %rbp
   movq %rsp, %rbp
-  call  my_get_interrupts_counter # counter++
-  movl %eax,%edi # first param to what_to_do is conter
-  call what_to_do # call what_to_do
-  movq %rax, %rcx # save output of what_to_do in rcx
-  cmp $0,%rax # if what_to_do output is zero
-  jne our_handler # if not zero
-  leave
-  jmp old_de_handler # if zero jump to old handler
-our_handler:
-  movq %rcx, %rax # move what_to_do output to rax
-  movq $-1, %rdx # move -1 to the remeinder
-  xor %r8, %r8
-  xor %rcx, %rcx
-  movq 8(%rbp),%r8 # move rip to r8
-  # movq (%rsp), %r8 # move rip to r8
-  while_f6_or_f7:
-  movb (%r8), %cl # cl=rbp first byte
-  cmp $0xf6, %cl # check if f6==cl
-  je advance_rip
-  cmp $0xf7, %cl # check if f7==cl
-  je advance_rip
-  inc %r8 # r8+=1 (byte)
-  jmp while_f6_or_f7
 
-  advance_rip:
-  xor %rax,%rax # reset rax
-  add $2, %r8 # rip += 2 bytes
-  movq %rax, 8(%rbp) # saves new rip in stack
+  incq (counter)
+  movq (counter), %rdi
+  call what_to_do # call what_to_do
+  movq %rax, %r9 # saves what_to_do return value t r9
+  cmp $0, %rax
+  je old_handler
+  movq 8(%rbp),%r8 # move rip to r8
+  addq $2, %r8 # rip += 2 bytes
+  movq %r8, 8(%rbp) # saves new rip in stack
+  movl $-1, %edx # move -1 to the remeinder
+  xorq %rax, %rax
   leave
-  # movq %r8, (%rsp) # saves new rip in stack
   iretq
 
+old_handler:
+  leave
+  jmp *old_de_handler
 
 my_get_interrupts_counter:
-  #TODO: implement :)
-  pushq %rbp
-  movq %rsp, %rbp
-  xor %eax, %eax
-  movl counter, %eax # eax=counter
-  inc %eax # eax++
-  movl %eax, counter # counter++
-  leave
+  xorq %rax, %rax
+  movq counter, %rax
   ret
